558f1c4f61b92c333802693fb795dd0c0c6882d8
Robert Norell <robert.norell@outlook.com>
2021-05-26 23:33:47 -0500

    This is the barest of implementations that make sense to do here.

--- FluentQuery/ISelectedDatabase.cs
+++ FluentQuery/ISelectedDatabase.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace FluentQuery
 {
     public interface ISelectedDatabase : IQueryBuilder
@@ -15,11 +18,28 @@ namespace FluentQuery
 
         public IExecutableQuery WithParameters(object parameters)
         {
+            if (parameters == null)
+            {
+                QueryParameters = null;
+                return ???;
+            }
+
+            QueryParameters = parameters is IDictionary<string, object> dictionaryParameters
+                ? dictionaryParameters
+                : ConvertObjectToDictionary(parameters);
+
+            return ???;
+        }
+
+        private IDictionary<string, object> ConvertObjectToDictionary(object parameters)
+        {
             throw new System.NotImplementedException();
         }
 
         public bool Exists { get; }
 
         public string Database { get; set; }
+
+        public IDictionary<string, object> QueryParameters { get; private set; }
     }
 }
