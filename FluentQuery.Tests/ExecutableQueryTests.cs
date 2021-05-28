94a07dc314f4870d3c0934075efe43269a705e17
Robert Norell <robert.norell@outlook.com>
2021-05-27 19:14:37 -0500

    Basic back-coverage.

--- FluentQuery.Tests/ExecutableQueryTests.cs
+++ FluentQuery.Tests/ExecutableQueryTests.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace FluentQuery.Tests
@@ -6,11 +9,43 @@ namespace FluentQuery.Tests
     public class ExecutableQueryTests
     {
         private ExecutableQuery _executableQuery;
+        private Dictionary<string, object> _keyValuePairParameters;
+        private object _savedObjectParameter;
 
         [SetUp]
         public void Setup()
         {
             _executableQuery = new ExecutableQuery();
+
+            _keyValuePairParameters = new Dictionary<string, object>
+            {
+                ["Key"] = "value",
+                [Guid.NewGuid().ToString()] = Guid.NewGuid()
+            };
+
+            _savedObjectParameter = Guid.NewGuid();
+        }
+
+        [Test]
+        public void WithParameters_should_save_an_existing_dictionary_directly()
+        {
+            _executableQuery.WithParameters(_keyValuePairParameters);
+
+            _executableQuery.QueryParameters
+                .Should().BeSameAs(_keyValuePairParameters);
+        }
+
+        [Test]
+        public void WithParameters_should_translate_an_existing_object_to_key_value_sets()
+        {
+            _executableQuery.WithParameters(new
+            {
+                Key = "value",
+                Parameter = _savedObjectParameter
+            });
+
+            _executableQuery.QueryParameters["Parameter"]
+                .Should().BeSameAs(_savedObjectParameter);
         }
     }
 }
